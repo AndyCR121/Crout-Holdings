d3b38c9524d4fa4f05fb4fd7fe708510439e31c7
AndyCR121 <120375474+AndyCR121@users.noreply.github.com>
2026-05-19 09:25:20 +0200

    Update app.component.ts

--- crout-holdings/src/app/app.component.ts
+++ crout-holdings/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DevNavComponent } from './components/dev-nav/dev-nav.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavComponent } from './components/nav/nav.component';
 
 @Component({
   selector: 'ch-root',
   standalone: true,
-  imports: [RouterOutlet, DevNavComponent, NavComponent, FooterComponent],
+  imports: [RouterOutlet, NavComponent, FooterComponent],
   template: `
     <!-- <ch-dev-nav></ch-dev-nav> -->
     <ch-nav></ch-nav>
