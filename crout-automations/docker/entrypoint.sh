517832edcd4862cad4e54f3f07d37a9f7e12d4e2
AndyCR121 <120375474+AndyCR121@users.noreply.github.com>
2026-07-23 03:12:56 +0200

    Major Hotfix!!! SITE OFFLINE FIX!!!

--- crout-automations/docker/entrypoint.sh
+++ crout-automations/docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eu
 
-: "${API_URL:?API_URL must be set}"
+API_URL="${API_URL:-/api}"
+export API_URL
 
 envsubst '${API_URL}' < /usr/share/nginx/html/env.template.js > /usr/share/nginx/html/env.js
